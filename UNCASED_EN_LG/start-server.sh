64ffae84968ea167bf0f3a46fc0900ef480e3835
Gaurav Vijay Vergiya <gaurav.vergiya@phenompeople.com>
2019-02-19 06:55:22 +0000

    -graph_tmp_dir ---> ${PROJECT_HOME}/tmp

--- UNCASED_EN_LG/start-server.sh
+++ UNCASED_EN_LG/start-server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 NUMBER_OF_PROCS=$(nproc)
-bert-serving-start -model_dir models/uncased_L-24_H-1024_A-16 -num_worker=${NUMBER_OF_PROCS} > ${PROJECT_HOME}/logs/bert-server.log
+bert-serving-start -model_dir models/uncased_L-24_H-1024_A-16 -graph_tmp_dir ${PROJECT_HOME}/tmp  -num_worker=${NUMBER_OF_PROCS} > ${PROJECT_HOME}/logs/bert-server.log
