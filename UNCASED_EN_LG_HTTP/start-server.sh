7d74795b9a1d94da5f2102a1f85688960e209deb
gaurav <gaura.vergiya@phenompeople.com>
2019-03-07 15:39:20 +0530

    added large model with http support

---
+++ UNCASED_EN_LG_HTTP/start-server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+NUMBER_OF_PROCS=$(nproc)
+NUMBER_OF_PROCS=$(($NUMBER_OF_PROCS-1))
+bert-serving-start -model_dir models/uncased_L-24_H-1024_A-16 -graph_tmp_dir ${PROJECT_HOME}/tmp  -num_worker=${NUMBER_OF_PROCS} -http_port 8080 > ${PROJECT_HOME}/logs/bert-server.log
