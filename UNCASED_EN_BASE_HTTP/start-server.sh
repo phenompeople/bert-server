dd799984e0bd5627fd68529a5cbaa74e5f834268
phenom_praveen_batchu <praveen.batchu@phenompeople.com>
2019-04-02 20:35:23 +0530

    UNCASED_EN_BASE_HTTP FOR SPX TEAM

---
+++ UNCASED_EN_BASE_HTTP/start-server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+NUMBER_OF_PROCS=$(nproc)
+NUMBER_OF_PROCS=$(($NUMBER_OF_PROCS-1))
+bert-serving-start -model_dir models/uncased_L-12_H-768_A-12 -graph_tmp_dir ${PROJECT_HOME}/tmp  -num_worker=${NUMBER_OF_PROCS} -http_port 8080 > ${PROJECT_HOME}/logs/bert-server.log
